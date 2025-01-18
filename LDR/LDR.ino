4ab48b39e7fd4df6466f586679cc62aca07ad4d4
delphi12 <32071881+delphi12@users.noreply.github.com>
2025-01-18 12:56:29 -0500

    Integrated_sensors

--- LDR/LDR.ino
+++ LDR/LDR.ino
@@ -1,12 +1,10 @@
 
 #define LDR_PIN A0
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin (9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   int ldrValue = analogRead(LDR_PIN);
   Serial.print("LDR Value: ");
   Serial.println(ldrValue);
---
+++ Multiple_sensors_Integrated/Integrated sensors_readme.md
@@ -0,0 +1,3 @@
+**Integrated LDR, Humidity and ultasonic sensors with Arduino:**
+
+![Image](https://github.com/user-attachments/assets/e70b6b0b-adde-4ac2-9ce4-0e0849d1971c)
